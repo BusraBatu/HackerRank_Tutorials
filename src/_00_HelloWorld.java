6b5a9c9b673054a07c7439964da900b462d79e9a
Busra <busraanac4@gmail.com>
2022-07-01 16:59:50 +0300

    01

--- src/_00_HelloWorld.java
+++ src/_00_HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _01_HelloWorld {
+public class _00_HelloWorld {
 
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
