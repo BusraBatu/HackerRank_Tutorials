67b312ee46517909e6db0d3067dcc5cc33175922
Busra <busraanac4@gmail.com>
2022-07-18 12:33:31 +0300

    17

--- src/_15_LinkedList.java
+++ src/_15_LinkedList.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+
 public class _15_LinkedList {
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+        if (head == null) {
+            return new Node(data);
+        } else {
+            Node x = head;
+            while (x.next != null) {
+                x = x.next;
+            }
+            x.next = new Node(data);
+            return head;
+        }
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
 }
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
--- src/_16_Exceptions_StringtoInteger.java
+++ src/_16_Exceptions_StringtoInteger.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class _16_Exceptions_StringtoInteger {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _16_Exceptions_StringtoInteger {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = bufferedReader.readLine();
+        try{
+            System.out.println(Integer.valueOf(S));
+        }catch(IllegalArgumentException e){
+            System.out.println("Bad String");
+        }
+        bufferedReader.close();
+    }
 }
